4361f85bb4ac87fa1f200b7b2206de5eaa35be71
Wessel <wessel_kroos@hotmail.com>
2018-01-13 02:53:22 +0100

    Added v2 stylesheet and cleaned up javascript code as well as performance improvements and javascript values for possible settings in the future

--- src/scripts/youtube-ambilight-v2.js
+++ src/scripts/youtube-ambilight-v2.js
@@ -15,53 +15,53 @@ $ = {
   }
 }
 
-HTMLElement.prototype.attr = function(name, value) {
-  if(typeof value === 'undefined') {
+HTMLElement.prototype.attr = function (name, value) {
+  if (typeof value === 'undefined') {
     return this.getAttribute(name)
   } else {
     this.setAttribute(name, value)
     return this
   }
 }
-HTMLElement.prototype.append = function(elem) {
-  if(typeof elem === 'string')
+HTMLElement.prototype.append = function (elem) {
+  if (typeof elem === 'string')
     elem = document.createTextNode(elem)
   this.appendChild(elem)
   return this
 }
-HTMLElement.prototype.appendTo = function(elem) {
+HTMLElement.prototype.appendTo = function (elem) {
   elem.append(this)
   return this
 }
 HTMLElement.prototype.prependChild = HTMLElement.prototype.prepend
-HTMLElement.prototype.prepend = function(elem) {
+HTMLElement.prototype.prepend = function (elem) {
   this.prependChild(elem)
   return this
 }
-HTMLElement.prototype.prependTo = function(elem) {
+HTMLElement.prototype.prependTo = function (elem) {
   elem.prepend(this)
   return this
 }
-HTMLElement.prototype.class = function(className) {
+HTMLElement.prototype.class = function (className) {
   const existingClasses = this.className.split(' ')
-  if(existingClasses.indexOf(className) === -1)
+  if (existingClasses.indexOf(className) === -1)
     this.className += ' ' + className
   return this
 }
-HTMLElement.prototype.removeClass = function(className) {
+HTMLElement.prototype.removeClass = function (className) {
   const classList = this.className.split(' ')
   const pos = classList.indexOf(className)
-  if(pos !== -1) {
+  if (pos !== -1) {
     classList.splice(pos, 1)
     this.className = classList.join(' ')
   }
   return this
 }
-HTMLElement.prototype.text = function(text) {
+HTMLElement.prototype.text = function (text) {
   this.innerText = text
   return this
 }
-addEventListenerPrototype = function(eventNames, callback) {
+addEventListenerPrototype = function (eventNames, callback) {
   var list = eventNames.split(' ')
   list.forEach((eventName) => {
     this.addEventListener(eventName, callback)
@@ -71,71 +71,53 @@ addEventListenerPrototype = function(eventNames, callback) {
 HTMLElement.prototype.on = addEventListenerPrototype
 Window.prototype.on = addEventListenerPrototype
 
-HTMLElement.prototype.offset = function() {
+HTMLElement.prototype.offset = function () {
   return this.getBoundingClientRect()
 }
 
 body = document.body
-  
-  
+
+
 
 //// Ambilight
 
 class Ambilight {
   constructor() {
-    this.initStyling()
-    
     this.playerOffset = {}
     this.srcVideoOffset = {}
 
+    this.isHidden = true
     this.ambiEnabled = true
     this.sizeInvalidated = true
-    
+
     this.p = null;
     this.a = null;
     this.isFullscreen = false
     this.videoFrameCount = 0
 
-    
-    this.barSizeW = (1 / 18) //Based on video width
-    this.barSizeH = this.barSizeW * (16/9)
-    this.strength = 6
+
+    this.scaleStep = (1 / 9)
+    this.strength = 8
     this.innerStrength = 1
     this.opacityStrength = 3
-    
+    this.edgeStrenght = 0.2;
+
     this.video_player = $.s("#player video")
     this.video_player.style.webkitFilter = 'contrast(115%)'
-    
+
     this.allContainer = document.createElement("div")
-    this.allContainer.style.position = 'absolute'
-    this.allContainer.style.left = '0'
-    this.allContainer.style.top = '0'
-    this.allContainer.style.width = '100%'
-    this.allContainer.style.height = '200%'
-    this.allContainer.style.overflow = 'hidden'
-    this.allContainer.style.backfaceVisibility = 'hidden'
-    this.allContainer.style.transform = 'translate3d(0,0,1)'
+    this.allContainer.class('ambilight')
     body.prepend(this.allContainer)
 
     this.ambilightContainer = document.createElement("div")
-    this.ambilightContainer.style.position = 'absolute'
-    this.ambilightContainer.style.pointerEvents = 'none'
-    this.ambilightContainer.style.webkitFilter = 'blur(4px)'
-    //this.ambilightContainer.style.backfaceVisibility = 'hidden'
-    //this.ambilightContainer.style.transform = 'translate3d(0,0,1)'
+    this.ambilightContainer.class('ambilight__container')
     this.allContainer.prepend(this.ambilightContainer)
 
-    this.playersContainer = document.createElement("div")
-    this.playersContainer.style.position = 'absolute'
-    this.playersContainer.style.width = '100%'
-    this.playersContainer.style.height = '100%'
-    this.playersContainer.style.webkitFilter = 'contrast(115%)'
-    //this.playersContainer.style.mixBlendMode = 'lighten'
-    //this.playersContainer.style.zIndex = 1
-    //this.playersContainer.style.backfaceVisibility = 'hidden'
-    //this.playersContainer.style.transform = 'translate3d(0,0,1)'
-    this.ambilightContainer.prepend(this.playersContainer)
-    
+    this.canvasList = document.createElement("div")
+    this.canvasList.class('ambilight__canvas-list')
+    this.canvasList.style.webkitFilter = 'contrast(115%)'
+    this.ambilightContainer.prepend(this.canvasList)
+
     this.ambilightContainers = []
     this.players = []
     const bufferElem = document.createElement("canvas")
@@ -145,115 +127,81 @@ class Ambilight {
       elem: bufferElem,
       ctx: bufferCtx
     }
-    
-    const videoBufferElem = document.createElement("canvas")
-    const videoBufferCtx = bufferElem.getContext("2d")
-    videoBufferCtx.imageSmoothingEnabled = false
-    this.videoBuffer = {
-      elem: videoBufferElem,
-      ctx: videoBufferCtx
+
+    let lastScale = {
+      x: 1,
+      y: 1
     }
-    
-    let lastScaleX = 1
-    let lastScaleY = 1
-    for(let i = 0; i < this.strength; i++) {
-      let pos = i - this.innerStrength
-      const scaleStepW = this.barSizeW * 2
-      const scaleStepH = this.barSizeH * 2
+
+    for (let i = 0; i < this.strength; i++) {
+      const pos = i - this.innerStrength
       let scaleX = 1
       let scaleY = 1
-      const nextScale = (prev, step, direction) => {
-        step = (step * prev * (1 + step))
-        return (direction > 0) ? prev + step : prev - step
+
+      if (pos > 0) {
+        scaleX = 1 + (this.scaleStep * pos)
+        scaleY = 1 + ((this.scaleStep * (16 / 9)) * pos)
       }
-      while(pos > 0) {
-        scaleX = nextScale(scaleX, scaleStepW, 1)
-        scaleY = nextScale(scaleY, scaleStepH, 1)
-        pos--
+
+      if (pos < 0) {
+        scaleX = 1 - (this.scaleStep * -pos)
+        scaleY = 1 - ((this.scaleStep * (16 / 9)) * -pos)
+        if (scaleX < 0) scaleX = 0
+        if (scaleY < 0) scaleY = 0
       }
-      while(pos < 0) {
-        scaleX = nextScale(scaleX, scaleStepW, -1)
-        scaleY = nextScale(scaleX, scaleStepW, -1)
-        //Todo: If inner we should add it to a different container.
-        //This player has no blur, but the innercontainer has an overlapping blur
-        //This way we retain the correct edge color without losing brightness and edge details
-        pos++
-      }
-      
-      const elem = $.create('canvas')
-      elem.style.position = 'absolute'
-      //elem.style.outline = '1px solid #fff'
-      elem.style.width = '100%'
-      elem.style.height = '100%'
-      //this.opacityStrength
-      //this.innerStrength
-      //Old opacity v2
-      //elem.style.opacity = Math.min((1 - ( Math.log((i - this.innerStrength - this.opacityStrength) * 100000000) / Math.log((this.strength + 1 - this.innerStrength) * 100000000) )), 1)
-      //if(i == this.strength - 1) {
-      //  elem.style.webkitFilter = 'blur(30px)'
-      //}
-      elem.style.imageRendering = 'pixelated'
-      const ctx = elem.getContext('2d')
+
+      const canvasContainer = $.create('div')
+      canvasContainer.class('ambilight__canvas-container')
+      canvasContainer.style.transform = `scale(${scaleX}, ${scaleY})`
+
+      const canvas = $.create('canvas')
+      canvas.class('ambilight__canvas')
+
+      const ctx = canvas.getContext('2d')
       ctx.imageSmoothingEnabled = false
-    
-      const container = $.create('div')
-      container.style.position = 'absolute'
-      //container.style.outline = '1px solid #fff'
-      container.style.width = '100%'
-      container.style.height = '100%'
-      container.style.transform = `scale(${scaleX}, ${scaleY})`
-      this.playersContainer.prepend(container)
-      container.prepend(elem)
-    
+
+      canvasContainer.prepend(canvas)
+      this.canvasList.prepend(canvasContainer)
+
       this.players.push({
-        elem: elem,
+        elem: canvas,
         ctx: ctx
       })
-      this.ambilightContainers.push(container)
-      lastScaleX = scaleX
-      lastScaleY = scaleY
+      this.ambilightContainers.push(canvasContainer)
+      lastScale.x = scaleX
+      lastScale.y = scaleY
     }
 
     const shadow = $.create('div')
-    shadow.style.position = 'absolute'
-    shadow.style.width = '100%'
-    shadow.style.height = '100%'
-    shadow.style.transform = `scale(${lastScaleX}, ${lastScaleY})`
-    const middleColor = '#777777'
-    shadow.style.background = `linear-gradient(to right, #111 0%, ${middleColor}00 15%, ${middleColor}00 85%, #111 100%), linear-gradient(to bottom, #111 10%, ${middleColor} 25%, ${middleColor} 75%, #111 90%)`
-    shadow.style.mixBlendMode = 'multiply'
+    shadow.class('ambilight__shadow')
     this.ambilightContainer.appendChild(shadow)
 
+    const shadowFade = $.create('div')
+    shadowFade.class('ambilight__shadow-fade')
+    shadowFade.style.transform = `scale(${lastScale.x}, ${lastScale.y})`
+    const middleColor = '#999999'
+    const edgeColor = '#000000'
+    shadowFade.style.background = `linear-gradient(to right,  ${edgeColor}  0%, ${middleColor}00 25%, ${middleColor}00 75%, ${edgeColor} 100%),
+                                   linear-gradient(to bottom, ${edgeColor}  0%, ${middleColor}   25%, ${middleColor}   75%, ${edgeColor} 100%)`
+    shadow.appendChild(shadowFade)
+
     const shadowCenter = $.create('div')
-    shadowCenter.style.position = 'absolute'
-    shadowCenter.style.width = '76%'
-    shadowCenter.style.height = '60%'
-    shadowCenter.style.top = '20%'
-    shadowCenter.style.left = '12%'
-    shadowCenter.style.background = '#fff'
+    shadowCenter.class('ambilight__shadow-center')
+    shadowCenter.style.transform = `scale(${1 + this.edgeStrenght}, ${1 + (this.edgeStrenght * (16 / 9))})`
+    shadowCenter.style.borderRadius = '10%'
     shadow.appendChild(shadowCenter)
 
 
-    $.s('.ytp-size-button').on('click', () => {
-      setTimeout(() => {
-        this.sizeInvalidated = true
-      }, 1)
-    })
-    
-    window.addEventListener('resize', () => {
-      this.sizeInvalidated = true
-    })
-
-    this.video_player.on('playing', () => {
-      this.sizeInvalidated = true
-    })
-    .on('timeupdate', () => {
-      if (this.srcVideoOffset.width !== this.video_player.videoWidth
-        || this.srcVideoOffset.height !== this.video_player.videoHeight) {
+    $.s('.ytp-size-button').on('click', () => setTimeout(this.invalidate.bind(this), 0))
+    window.addEventListener('resize', this.invalidate.bind(this))
+    this.video_player.on('playing', this.invalidate.bind(this))
+      .on('timeupdate', () => {
+        if (this.srcVideoOffset.width !== this.video_player.videoWidth
+          || this.srcVideoOffset.height !== this.video_player.videoHeight) {
           this.sizeInvalidated = true
-      }
-    })
-    .on('seeked', () => this.resetVideoFrameCounter())
+        }
+      })
+      .on('seeked', () => this.resetVideoFrameCounter())
 
     this.scheduleNextFrame()
   }
@@ -261,48 +209,20 @@ class Ambilight {
   resetVideoFrameCounter() {
     this.videoFrameCount = 0
   }
-
-  initStyling() {
-    const html = $.s('html')
-    html.style.background = '#000'
-    body.style.background = '#000'
-    
-    const ytHead = $.s('ytd-masthead')
-    ytHead.style.background = 'none'
-    const searchInput = $.s('#container.ytd-searchbox')
-    searchInput.style.background = 'none'
-    //.ytd-searchbox #search::-webkit-input-placeholder {
-    //  color: var(--ytd-searchbox-legacy-border-color);
-    //}
-    const searchBtn = $.s('#search-icon-legacy')
-    searchBtn.style.background = 'none'
-    const searchForm = $.s('#search-form')
-    searchForm.style.opacity = .5
-    
-    const watch = $.s('ytd-watch')
-    watch.style.background = 'none'
-    
-    const app = $.s('ytd-app')
-    app.style.background = 'none'
-    //app.style.zIndex = 2
-    
-    const moviePlayer = $.s('#movie_player')
-    moviePlayer.style.background = 'none'
-    
-    const playerContainer = $.s('#player-container')
-    playerContainer.style.background = 'none'
+  invalidate() {
+    this.sizeInvalidated = true
   }
 
   updateSizes() {
     //console.log('resizing')
     this.playerOffset = this.video_player.offset()
-    if(this.playerOffset.top < 0) return false //Not ready
+    if (this.playerOffset.top === undefined) return false //Not ready
 
     this.srcVideoOffset = {
       width: this.video_player.videoWidth,
       height: this.video_player.videoHeight
     }
-    
+
     //console.log(`srcVideoOffset ${this.srcVideoOffset.width} x ${this.srcVideoOffset.height}`)
     const scale = Math.ceil(Math.max(this.srcVideoOffset.height, 360) / 360)
     this.p = {
@@ -311,137 +231,103 @@ class Ambilight {
     }
     //console.log(`p ${this.p.w} x ${this.p.h} | scale ${scale}`)
     //console.log(`${scale} | ${this.p.w} | ${this.p.h}`)
-  
-    
+
+
     this.isFullscreen = !!$.s('.ytp-fullscreen')
     //console.log('Fullscreen: ' + this.isFullscreen)
 
-    //this.ambilightContainer.style.webkitFilter = `blur(${this.playerOffset.height/100 * 10}px)`
     this.ambilightContainer.style.top = this.playerOffset.top + 'px'
     this.ambilightContainer.style.left = this.playerOffset.left + 'px'
     this.ambilightContainer.style.width = this.playerOffset.width + 'px'
     this.ambilightContainer.style.height = (this.playerOffset.height + window.pageYOffset) + 'px'
-    
-    this.ambilightContainer.style.webkitFilter = `blur(${this.playerOffset.height/13}px)`
-      
+
+    this.ambilightContainer.style.webkitFilter = `blur(${this.playerOffset.height / 13}px)`
+
     this.players.forEach((player) => {
       player.elem.width = this.p.w
       player.elem.height = this.p.h
-      //player.elem.style.webkitFilter = `blur(${this.playerOffset.height/100 * 3}`
-      //player.elem.style.webkitFilter = `blur(${this.playerOffset.height/100 * Math.max((5 + 3 * (1 - Math.max(player.elem.style.opacity, .1))), 5)}px)`
       player.ctx = player.elem.getContext('2d')
-      this.videoBuffer.elem.width = this.srcVideoOffset.width
-      this.videoBuffer.elem.height = this.srcVideoOffset.height
-      this.videoBuffer.ctx = this.videoBuffer.elem.getContext('2d')
       this.buffer.elem.width = this.p.w
       this.buffer.elem.height = this.p.h
       this.buffer.ctx = this.buffer.elem.getContext('2d')
     })
-  
-    /*const edge = {
-      w: this.p.w * this.barSizeW,
-      h: this.p.h * this.barSizeH
-    }
-    this.a = {
-      1: {
-        x: 0,
-        y: 0,
-        w: this.p.w,
-        h: edge.h
-      },
-      2: {
-        x: 0,
-        y: this.p.h - edge.h,
-        w: this.p.w,
-        h: edge.h
-      },
-      3: {
-        x: 0,
-        y: edge.h,
-        w: edge.w,
-        h: this.p.h - edge.h - edge.h
-      },
-      4: {
-        x: this.p.w - edge.w,
-        y: edge.h,
-        w: edge.w,
-        h: this.p.h - edge.h - edge.h
-      }
-    }*/
-  
+
     this.sizeInvalidated = false
     this.resetVideoFrameCounter()
     return true;
   }
-  
+
   scheduleNextFrame() {
     var playerOffset = this.video_player.offset()
     this.sizeInvalidated = !(this.ambilightContainer.style.width == this.playerOffset.width + 'px')
     window.requestAnimationFrame(() => this.drawAmbilight())
   }
-  
-  drawAmbilight() {
-    if(!this.ambiEnabled) return this.scheduleNextFrame()
 
-    if((this.sizeInvalidated && !this.updateSizes()) ||
+  drawAmbilight() {
+    if (!this.ambiEnabled) return this.scheduleNextFrame()
+
+    if ((this.sizeInvalidated && !this.updateSizes()) ||
       this.isFullscreen) {
       return this.scheduleNextFrame()
     }
 
-    if(this.video_player.currentTime == 0) {
+    if (this.video_player.currentTime == 0) {
       this.hide()
       return this.scheduleNextFrame()
     }
-    if(this.isHidden) {
-      window.ambilight.ambilightContainer.style.display = ''
+    if (this.isHidden) {
       this.isHidden = false
+      this.ambilightContainer.style.opacity = '1'
     }
-  
-    
+
+
     var newFrameCount = this.video_player.webkitDecodedFrameCount + this.video_player.webkitDroppedFrameCount
-    if(this.videoFrameCount == newFrameCount) {
+    if (this.videoFrameCount == newFrameCount) {
       return this.scheduleNextFrame()
     }
     this.videoFrameCount = newFrameCount
 
-    this.videoBuffer.ctx.drawImage(this.video_player, 0, 0)
-    this.buffer.ctx.drawImage(this.videoBuffer.elem, 0, 0, this.p.w, this.p.h)
+    this.buffer.ctx.drawImage(this.video_player, 0, 0, this.p.w, this.p.h)
     this.players.forEach((player) => {
       player.ctx.drawImage(this.buffer.elem, 0, 0)
     })
-  
+
     this.scheduleNextFrame()
   }
 
   disable() {
+    if (!this.ambiEnabled) return
     this.ambiEnabled = false
     this.hide()
   }
 
   hide() {
+    if (this.isHidden) return
     this.isHidden = true
-    this.ambilightContainer.style.display = 'none'
-    this.players.forEach((player) => {
-      player.ctx.clearRect(0,0,player.elem.width, player.elem.height)
-    })
+    this.ambilightContainer.style.opacity = '0'
+    setTimeout(() => {
+      this.players.forEach((player) => {
+        player.ctx.clearRect(0, 0, player.elem.width, player.elem.height)
+      })
+    }, 500)
   }
 }
 
 
 enableAmbilight = () => {
   const isVideoPage = window.location.href.indexOf('watch?v=') != -1
-  if(window.ambilight) {
-    if(!isVideoPage) {
-      if(window.ambilight.ambiEnabled) {
+  if (window.ambilight) {
+    if (!isVideoPage) {
+      if (window.ambilight.ambiEnabled) {
         window.ambilight.disable()
       }
     } else {
-      if(!window.ambilight.ambiEnabled) {
+      if (!window.ambilight.ambiEnabled) {
         window.ambilight.ambiEnabled = true
       }
     }
-  } else if(isVideoPage) {
-    if(!$.s('ytd-masthead')) return //Not ready
+  } else if (isVideoPage) {
+    if (!$.s('ytd-masthead')) return //Not ready
     window.ambilight = new Ambilight()
   }
 }
