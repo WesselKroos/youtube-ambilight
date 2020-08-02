859c9d0a72f64d7ca9253732ee84123b04328629
Wessel Kroos <wessel_kroos@hotmail.com>
2020-08-02 21:37:51 +0200

    Re-allow black bars detection on non-chrome browsers when the video src is not from youtube.com

--- src/scripts/youtube-ambilight.js
+++ src/scripts/youtube-ambilight.js
@@ -160,7 +160,7 @@ class Ambilight {
   }
 
   initGetImageDataAllowed() {
-    this.getImageDataAllowed = (this.videoElem.src && this.videoElem.src.indexOf('youtube.com') !== -1)
+    this.getImageDataAllowed = (!window.chrome || (this.videoElem.src && this.videoElem.src.indexOf('youtube.com') !== -1))
 
     const settings = [
       $.s(`#setting-detectHorizontalBarSizeEnabled`),
