1dd83513a3eeca98427ee8df811e526fd8e331bc
Wessel Kroos <wessel_kroos@hotmail.com>
2022-03-12 20:30:35 +0100

    More efficient webgl canvas and shadow resizing

--- src/scripts/libs/projector-webgl.js
+++ src/scripts/libs/projector-webgl.js
@@ -1,7 +1,9 @@
 import ProjectorShadow from './projector-shadow'
 
 export default class ProjectorWebGL {
+  scales = []
   levels = 1
+  maxScalesLength = 103
 
   constructor(containerElem) {
     this.containerElem = containerElem
@@ -50,35 +52,30 @@ export default class ProjectorWebGL {
   }
 
   rescale(scales, lastScale, projectorSize, settings) {
-    try {
-      const scalesLengthUnchanged = this.scales?.length === scales.length
-      this.scale = scales[scales.length - 1]
-      this.scales = scales.map(({x, y}) => ({
-        x: this.scale.x / x,
-        y: this.scale.y / y
-      }))
+    this.scale = scales[scales.length - 1]
+    this.scales = scales.map(({x, y}) => ({
+      x: this.scale.x / x,
+      y: this.scale.y / y
+    }))
 
-      // Todo: For performance calculate the height as if horizontalBarsClipScaleY does not exist
-      const width = Math.floor(this.width * this.scale.x)
-      const height = Math.floor(this.height * this.scale.y)
-      const blurPx = Math.round(settings.blur * (this.height / 512) * 1.275)
-      this.blurBound = blurPx * 2.64;
+    // Todo: For performance calculate the height as if horizontalBarsClipScaleY does not exist
+    const width = Math.floor(this.width * this.scale.x)
+    const height = Math.floor(this.height * this.scale.y)
+    const blurPx = Math.round(settings.blur * (this.height / 512) * 1.275)
+    this.blurBound = blurPx * 2.64;
+    
+    this.shadow.rescale(lastScale, projectorSize, settings)
 
-      this.blurCanvas.style.transform = `scale(${this.scale.x + ((this.blurBound * 2) / this.width)}, ${this.scale.y + ((this.blurBound * 2) / this.height)})`
+    this.canvas.width = width
+    this.canvas.height = height
 
-      this.canvas.width = width
-      this.blurCanvas.width = width + this.blurBound * 2
-      this.canvas.height = height
-      this.blurCanvas.height = height + this.blurBound * 2
-      
-      this.shadow.rescale(lastScale, projectorSize, settings)
+    this.blurCanvas.width = width + this.blurBound * 2
+    this.blurCanvas.height = height + this.blurBound * 2
+    this.blurCanvas.style.transform = `scale(${this.scale.x + ((this.blurBound * 2) / this.width)}, ${this.scale.y + ((this.blurBound * 2) / this.height)})`
+    
+    this.updateCtx()
 
-      this.updateCtx(scalesLengthUnchanged)
-
-      this.blurCtx.filter = `blur(${blurPx}px)`
-    } catch(ex) {
-      console.warn(ex)
-    }
+    this.blurCtx.filter = `blur(${blurPx}px)`
   }
 
   draw(src) {
@@ -134,124 +131,126 @@ export default class ProjectorWebGL {
     this.ctx.shaderSource(vertexShader, vertexShaderSrc);
     this.ctx.compileShader(vertexShader);
     if (!this.ctx.getShaderParameter(vertexShader, this.ctx.COMPILE_STATUS)) {
-      console.error('vertexShader', this.ctx.getShaderInfoLog(vertexShader));
-      return;
+      throw new Error(`vertexShader: ${this.ctx.getShaderInfoLog(vertexShader)}`);
     }
     this.ctx.attachShader(this.program, vertexShader);
+    
+    // Todo: Replace for loop with a direct [x,y] to scale conversion (GPU 65% -> 45%)
+    const fragmentShaderSrc = `
+      precision lowp float;
+      varying vec2 fUV;
+      uniform sampler2D ambilightSampler;
+      uniform sampler2D shadowSampler;
+      uniform int fScalesLength;
+      uniform vec2 fScales[${this.maxScalesLength}];
+      uniform vec2 fScalesMinus[${this.maxScalesLength}];
+      uniform vec4 fBorderColor;
+    
+      vec4 multiTexture(sampler2D sampler, vec2 uv) {
+        for (int i = 0; i < ${this.maxScalesLength}; i++) {
+          if (i == fScalesLength) break;
+          vec2 scaledUV = vec2(
+            uv[0] * fScales[i][0] - fScalesMinus[i][0],
+            uv[1] * fScales[i][1] - fScalesMinus[i][1]
+          );
+          if (
+            scaledUV[0] > 0. && scaledUV[0] < 1. &&
+            scaledUV[1] > 0. && scaledUV[1] < 1.
+          ) {
+            return texture2D(sampler, scaledUV);
+          }
+        }
+        return fBorderColor;
+      }
+      
+      void main(void) {
+        vec4 ambilight = multiTexture(ambilightSampler, fUV);
+        float shadowAlpha = texture2D(shadowSampler, fUV).a;
+        ambilight[3] = 1. - shadowAlpha;
+        gl_FragColor = ambilight;
+      }
+    `;
+    const fragmentShader = this.ctx.createShader(this.ctx.FRAGMENT_SHADER);
+    this.ctx.shaderSource(fragmentShader, fragmentShaderSrc);
+    this.ctx.compileShader(fragmentShader);
+    if (!this.ctx.getShaderParameter(fragmentShader, this.ctx.COMPILE_STATUS)) {
+      throw new Error(`fragmentShader: ${this.ctx.getShaderInfoLog(fragmentShader)}`);
+    }
+    this.ctx.attachShader(this.program, fragmentShader);
+    
+    // Program
+    this.ctx.linkProgram(this.program);
+    if (!this.ctx.getProgramParameter(this.program, this.ctx.LINK_STATUS)) {
+      throw new Error(`program: ${this.ctx.getProgramInfoLog(this.program)}`);
+    }
+    this.ctx.validateProgram(this.program);
+    if(!this.ctx.getProgramParameter(this.program, this.ctx.VALIDATE_STATUS)) {
+      throw new Error(`program: ${this.ctx.getProgramInfoLog(this.program)}`);
+    }
+    this.ctx.useProgram(this.program);
+
+    // Buffers
+    const vUVBuffer = this.ctx.createBuffer();
+    this.ctx.bindBuffer(this.ctx.ARRAY_BUFFER, vUVBuffer);
+    this.ctx.bufferData(this.ctx.ARRAY_BUFFER, new Float32Array([
+      0, 1, 
+      0, 0, 
+      1, 0, 
+      1, 1
+    ]), this.ctx.STATIC_DRAW);
+    const vUVLoc = this.ctx.getAttribLocation(this.program, 'vUV');
+    this.ctx.vertexAttribPointer(vUVLoc, 2, this.ctx.FLOAT, false, 2 * Float32Array.BYTES_PER_ELEMENT, 0);
+    this.ctx.enableVertexAttribArray(vUVLoc);
+
+    const vPositionBuffer = this.ctx.createBuffer();
+    this.ctx.bindBuffer(this.ctx.ARRAY_BUFFER, vPositionBuffer);
+    this.ctx.bufferData(this.ctx.ARRAY_BUFFER, new Float32Array([
+      -1, 1, 
+      -1, -1, 
+      1, -1, 
+      1, 1
+    ]), this.ctx.STATIC_DRAW);
+    const vPositionLoc = this.ctx.getAttribLocation(this.program, 'vPosition'); 
+    this.ctx.vertexAttribPointer(vPositionLoc, 2, this.ctx.FLOAT, false, 2 * Float32Array.BYTES_PER_ELEMENT, 0);
+    this.ctx.enableVertexAttribArray(vPositionLoc);
+      
+    const fBorderColorLoc = this.ctx.getUniformLocation(this.program, 'fBorderColor');
+    const borderColor = new Float32Array([0, 0, 0, 0]);
+    this.ctx.uniform4fv(fBorderColorLoc, borderColor);
+
+    const shadowSamplerLoc = this.ctx.getUniformLocation(this.program, "shadowSampler");
+    this.ctx.uniform1i(shadowSamplerLoc, 0);
+
+    const ambilightSamplerLoc = this.ctx.getUniformLocation(this.program, "ambilightSampler");
+    this.ctx.uniform1i(ambilightSamplerLoc, 1);
+    
+    this.fScalesLengthLoc = this.ctx.getUniformLocation(this.program, 'fScalesLength');
+    this.fScalesLoc = this.ctx.getUniformLocation(this.program, 'fScales');
+    this.fScalesMinusLoc = this.ctx.getUniformLocation(this.program, 'fScalesMinus');
   }
 
-  updateCtx(scalesLengthUnchanged = false) {
-    if(!this.fragmentShader || !scalesLengthUnchanged) {
-      if (this.fragmentShader) {
-        this.ctx.detachShader(this.program, this.fragmentShader);
-      }
-
-      // Todo: Replace for loop with a direct [x,y] to scale conversion (GPU 65% -> 45%)
-      const fragmentShaderSrc = `
-        precision lowp float;
-        varying vec2 fUV;
-        uniform sampler2D ambilightSampler;
-        uniform sampler2D shadowSampler;
-        uniform vec2 fScales[${this.scales.length}];
-        uniform vec2 fScalesMinus[${this.scales.length}];
-        uniform vec4 fBorderColor;
-      
-        vec4 multiTexture(sampler2D sampler, vec2 uv) {
-          for (int i = 0; i < ${this.scales.length}; i++) {
-            vec2 scaledUV = vec2(
-              uv[0] * fScales[i][0] - fScalesMinus[i][0],
-              uv[1] * fScales[i][1] - fScalesMinus[i][1]
-            );
-            if (
-              scaledUV[0] > 0. && scaledUV[0] < 1. &&
-              scaledUV[1] > 0. && scaledUV[1] < 1.
-            ) {
-              return texture2D(sampler, scaledUV);
-            }
-          }
-          return fBorderColor;
-        }
-        
-        void main(void) {
-          vec4 ambilight = multiTexture(ambilightSampler, fUV);
-          float shadowAlpha = texture2D(shadowSampler, fUV).a;
-          ambilight[3] = 1. - shadowAlpha;
-          gl_FragColor = ambilight;
-        }
-      `;
-      const fragmentShader = this.ctx.createShader(this.ctx.FRAGMENT_SHADER);
-      this.ctx.shaderSource(fragmentShader, fragmentShaderSrc);
-      this.ctx.compileShader(fragmentShader);
-      if (!this.ctx.getShaderParameter(fragmentShader, this.ctx.COMPILE_STATUS)) {
-        console.error('fragmentShader', this.ctx.getShaderInfoLog(fragmentShader));
-        return;
-      }
-      this.ctx.attachShader(this.program, fragmentShader);
-      this.fragmentShader = fragmentShader;
-      
-      this.ctx.linkProgram(this.program);
-      if (!this.ctx.getProgramParameter(this.program, this.ctx.LINK_STATUS)) {
-        console.error('program', this.ctx.getProgramInfoLog(this.program));
-        return;
-      }
-      this.ctx.validateProgram(this.program);
-      if(!this.ctx.getProgramParameter(this.program, this.ctx.VALIDATE_STATUS)) {
-        console.error('program', this.ctx.getProgramInfoLog(this.program));
-        return;
-      }
-      this.ctx.useProgram(this.program);
-
-      // Buffers
-      const vUVBuffer = this.ctx.createBuffer();
-      this.ctx.bindBuffer(this.ctx.ARRAY_BUFFER, vUVBuffer);
-      this.ctx.bufferData(this.ctx.ARRAY_BUFFER, new Float32Array([
-        0, 1, 
-        0, 0, 
-        1, 0, 
-        1, 1
-      ]), this.ctx.STATIC_DRAW);
-      const vUVLoc = this.ctx.getAttribLocation(this.program, 'vUV');
-      this.ctx.vertexAttribPointer(vUVLoc, 2, this.ctx.FLOAT, false, 2 * Float32Array.BYTES_PER_ELEMENT, 0);
-      this.ctx.enableVertexAttribArray(vUVLoc);
-
-      const vPositionBuffer = this.ctx.createBuffer();
-      this.ctx.bindBuffer(this.ctx.ARRAY_BUFFER, vPositionBuffer);
-      this.ctx.bufferData(this.ctx.ARRAY_BUFFER, new Float32Array([
-        -1, 1, 
-        -1, -1, 
-        1, -1, 
-        1, 1
-      ]), this.ctx.STATIC_DRAW);
-      const vPositionLoc = this.ctx.getAttribLocation(this.program, 'vPosition'); 
-      this.ctx.vertexAttribPointer(vPositionLoc, 2, this.ctx.FLOAT, false, 2 * Float32Array.BYTES_PER_ELEMENT, 0);
-      this.ctx.enableVertexAttribArray(vPositionLoc);
-        
-      const fBorderColorLoc = this.ctx.getUniformLocation(this.program, 'fBorderColor');
-      const borderColor = new Float32Array([0, 0, 0, 0]);
-      this.ctx.uniform4fv(fBorderColorLoc, borderColor);
-
-      const shadowSamplerLoc = this.ctx.getUniformLocation(this.program, "shadowSampler");
-      this.ctx.uniform1i(shadowSamplerLoc, 0);
-  
-      const ambilightSamplerLoc = this.ctx.getUniformLocation(this.program, "ambilightSampler");
-      this.ctx.uniform1i(ambilightSamplerLoc, 1);
-      
-      this.fScalesLoc = this.ctx.getUniformLocation(this.program, 'fScales');
-      this.fScalesMinusLoc = this.ctx.getUniformLocation(this.program, 'fScalesMinus');
+  updateCtx() {
+    const fScalesLength = this.scales.length;
+    const fScalesLengthChanged = this.fScalesLength !== fScalesLength;
+    if(fScalesLengthChanged) {
+      this.fScalesLength = fScalesLength;
+      this.ctx.uniform1i(this.fScalesLengthLoc, fScalesLength);
     }
 
-    const fScales = new Float32Array(this.scales.map(({ x, y }) => [x, y]).flat());
-    this.ctx.uniform2fv(this.fScalesLoc, fScales);
-
-    const fScalesMinus = new Float32Array(this.scales.map(({ x, y }) => [((x - 1) / 2), ((y - 1) / 2)]).flat());
-    this.ctx.uniform2fv(this.fScalesMinusLoc, fScalesMinus);
+    const fScales = this.scales.map(({ x, y }) => [x, y]).flat();
+    if(fScalesLengthChanged || fScales.some((fScale, i) => fScale !== this.fScales[i])) {
+      this.fScales = fScales;
+      this.ctx.uniform2fv(this.fScalesLoc, new Float32Array(fScales));
+      const fScalesMinus = fScales.map(i => (i - 1) / 2);
+      this.ctx.uniform2fv(this.fScalesMinusLoc, new Float32Array(fScalesMinus));
+    }
 
     this.ctx.activeTexture(this.ctx.TEXTURE0);
     this.ctx.texImage2D(this.ctx.TEXTURE_2D, 0, this.ctx.RGBA, this.ctx.RGBA, this.ctx.UNSIGNED_BYTE, this.shadow.elem);
     this.ctx.activeTexture(this.ctx.TEXTURE1);
 
-    if (!this.viewport || this.viewport.x !== 0 || this.viewport.y !== 0 || this.viewport.width !== this.ctx.drawingBufferWidth || this.viewport.height !== this.ctx.drawingBufferHeight) {
-      this.viewport = { x: 0, y: 0, width: this.ctx.drawingBufferWidth, height: this.ctx.drawingBufferHeight };
+    if (!this.viewport || this.viewport.width !== this.ctx.drawingBufferWidth || this.viewport.height !== this.ctx.drawingBufferHeight) {
+      this.viewport = { width: this.ctx.drawingBufferWidth, height: this.ctx.drawingBufferHeight };
       this.ctx.viewport(0, 0, this.ctx.drawingBufferWidth, this.ctx.drawingBufferHeight);
     }
   }
