f2fe1009d69f2f2d3a2e49d0c0799f300e46b2a5
Wessel Kroos <wessel_kroos@hotmail.com>
2022-11-05 22:56:42 +0100

    ContextMenu Donation button

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -15,3 +15,15 @@ chrome.runtime.onInstalled.addListener(function (details) {
 chrome.browserAction.onClicked.addListener(function (tab) {
   chrome.runtime.openOptionsPage()
 })
+
+chrome.contextMenus.create({
+  id: 'donate',
+  title: '💳 Donate',
+  contexts: ['browser_action']
+})
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  chrome.tabs.create({
+    url: 'https://ko-fi.com/G2G59EK8L'
+  })
+})
