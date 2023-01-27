2c6c07c2d1846c86e1e2ac5d2e6c1482a9f1c102
Wessel Kroos <wessel_kroos@hotmail.com>
2023-01-27 20:23:58 +0100

    Better black bar detection base offset

--- src/scripts/libs/bar-detection.js
+++ src/scripts/libs/bar-detection.js
@@ -143,7 +143,7 @@ const workerCode = function () {
       const maxDeviation = Math.abs(Math.max(...closestSizes) - Math.min(...closestSizes))
       const allowed = maxSize * (0.0125 * scale)
       const deviationAllowed = (maxDeviation <= allowed)
-      const baseOffsetPercentage = (0.4 * scale)
+      const baseOffsetPercentage = (0.7 * ((1 + scale) / 2))
       const maxPercentage = 36
 
       let size = 0;
