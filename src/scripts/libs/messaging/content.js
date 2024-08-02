900947964fd92164ac2dc482467649060e775b83
Wessel Kroos <wessel_kroos@hotmail.com>
2024-08-02 19:19:50 +0200

    More efficient messaging logic

--- src/scripts/libs/messaging/content.js
+++ src/scripts/libs/messaging/content.js
@@ -2,29 +2,61 @@ import { appendErrorStack, wrapErrorHandler } from '../generic'
 import { extensionId, isSameWindowMessage } from './utils'
 
 class ContentScript {
-  addMessageListener(type, handler) {
+  globalListener;
+  listeners = []
+
+  addMessageListener = (type, handler) => {
+    // console.log('content addMessageListener', type)
+    if(!this.globalListener) {
+      // console.log('content addMessageListenerGlobal')
+      this.globalListener = wrapErrorHandler(function contentScriptMessageListenerGlobal(event) {
+        // console.log('content message? global', event.data?.type)
+        if (
+          !isSameWindowMessage ||
+          event.data?.contentScript !== extensionId ||
+          !event.data?.type
+        ) return
+
+        for(const listener of this.listeners) {
+          listener(event)
+        }
+      }.bind(this), true)
+      window.addEventListener('message', this.globalListener, true)
+    }
+
     const listener = wrapErrorHandler(function contentScriptMessageListener(event) {
-      if (
-        !isSameWindowMessage ||
-        event.data?.contentScript !== extensionId ||
-        event.data?.type !== type
-      ) return
+      // console.log('content message?', type, event.data?.type)
+      if(event.data.type !== type) return
+
+      // console.log('content message!', type)
       handler(event.data?.message)
-    })
-    window.addEventListener('message', listener, true)
+    }.bind(this))
+
+    this.listeners.push(listener)
     return listener
   }
 
-  removeMessageListener(listener) {
-    window.removeEventListener('message', listener)
+  removeMessageListener = (listener) => {
+    const index = this.listeners.indexOf(listener)
+    if(index !== -1) {
+      // console.log('content removeMessageListener', index, listener)
+      this.listeners.splice(index, 1)
+    }
+
+    if(this.globalListener && this.listeners.length === 0) {
+      // console.log('content removeMessageListenerGlobal', this.globalListener)
+      window.removeEventListener('message', this.globalListener, true)
+      this.globalListener = undefined
+    }
   }
 
-  postMessage(type, message) {
+  postMessage = (type, message, transfer) => {
+    // console.log('content postMessage', type)
     window.postMessage({
       message,
       type,
       injectedScript: extensionId
-    }, origin)
+    }, origin, transfer)
   }
 
   getStorageEntriesId = 0
@@ -43,7 +75,7 @@ class ContentScript {
               this.removeMessageListener(listener)
               
               if(error &&
-                (throwOnUninstalled || !(error.message === 'uninstalled' || error.message.includes('QuotaExceededError')))
+                (throwOnUninstalled || !(error.message === 'uninstalled' || error.message?.includes('QuotaExceededError')))
               ) throw error
 
               resolve(valueOrValues)
@@ -80,7 +112,7 @@ class ContentScript {
               this.removeMessageListener(listener)
 
               if(error &&
-                (throwOnUninstalled || !(error.message === 'uninstalled' || error.message.includes('QuotaExceededError')))
+                (throwOnUninstalled || !(error.message === 'uninstalled' || error.message?.includes('QuotaExceededError')))
               ) throw error
 
               resolve()
--- src/scripts/libs/messaging/injected.js
+++ src/scripts/libs/messaging/injected.js
@@ -2,21 +2,52 @@ import { wrapErrorHandler } from '../generic'
 import { extensionId, isSameWindowMessage } from './utils'
 
 class InjectedScript {
+  globalListener;
+  listeners = []
+
   addMessageListener(type, handler) {
-    const listener = wrapErrorHandler(function InjectedScriptMessageListener(event) {
-      if (
-        !isSameWindowMessage ||
-        event.data?.injectedScript !== extensionId ||
-        event.data?.type !== type
-      ) return
+    // console.log('injected addMessageListener', type)
+    if(!this.globalListener) {
+      // console.log('injected addMessageListenerGlobal')
+      this.globalListener = wrapErrorHandler(function injectedScriptMessageListenerGlobal(event) {
+        // console.log('injected message? global', event.data?.type)
+        if (
+          !isSameWindowMessage ||
+          event.data?.injectedScript !== extensionId ||
+          !event.data?.type
+        ) return
+
+        for(const listener of this.listeners) {
+          listener(event)
+        }
+      }.bind(this), true)
+      window.addEventListener('message', this.globalListener, true)
+    }
+
+    const listener = wrapErrorHandler(function injectedScriptMessageListener(event) {
+      // console.log('injected message?', type, event.data?.type)
+      if(event.data.type !== type) return
+
+      // console.log('injected message!', type)
       handler(event.data?.message)
-    }.bind(this), true)
-    window.addEventListener('message', listener, true)
+    }.bind(this))
+
+    this.listeners.push(listener)
     return listener
   }
 
   removeMessageListener(listener) {
-    window.removeEventListener('message', listener)
+    const index = this.listeners.indexOf(listener)
+    if(index !== -1) {
+      // console.log('injected removeMessageListener', index, listener)
+      this.listeners.splice(index, 1)
+    }
+
+    if(this.globalListener && this.listeners.length === 0) {
+      // console.log('injected removeMessageListenerGlobal', this.globalListener)
+      window.removeEventListener('message', this.globalListener, true)
+      this.globalListener = undefined
+    }
   }
 
   postMessage(type, message) {
