6a2e16f633b9d9eda632d013837d900eee36eb5b
Wessel Kroos <wessel_kroos@hotmail.com>
2025-06-29 21:48:52 +0200

    update to dotenvx

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,12 @@ import fs from 'fs';
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import eslint from '@rollup/plugin-eslint';
-import dotenv from 'dotenv';
-dotenv.config();
-dotenv.config({
+import dotenvx from '@dotenvx/dotenvx';
+dotenvx.config();
+dotenvx.config({
   path: '.env.local',
   override: true,
+  overload: true,
 });
 // import { sentryRollupPlugin } from '@sentry/rollup-plugin';
 // import packageJson from './package.json' with { type: 'json' };
